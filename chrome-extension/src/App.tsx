36ec1daff1a40809a65439a88a4d80976fd140b7
KymaiselHunter <kyle.bautista78@myhunter.cuny.edu>
2025-09-08 14:31:12 -0400

    moved app into a router

--- chrome-extension/src/App.tsx
+++ chrome-extension/src/App.tsx
@@ -1,47 +1,16 @@
-// import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  	// const [count, setCount] = useState(0)
-  	const onclick = async () => {
-    	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
-		chrome.scripting.executeScript({
-			target: { tabId: tab.id! },
-			func: () => {
-				alert('hello worl');
-				document.body.style.backgroundColor = 'red';
-			}
-		});
-	}
-	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-				<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-				<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={()=> console.log('print')}>
-				{/* count is {count} */}print
-				</button>
-				<button onClick={()=> onclick()}>
-				{/* count is {count} */}alert
-				</button>
-				<p>
-				Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
-	)
-}
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
 
-export default App
+function App() {
+  	
+	return (
+		<Routes> 
+    		<Route path="/" element={<Home />} />
+			{/* <Route path="/club" element={<Club />} />
+			<Route path="/event" element={<Event />} />
+			<Route path="/auth" element={<Auth />} /> */}
+    	</Routes>
+	)
+} export default App
