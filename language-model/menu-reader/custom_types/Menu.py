a67549916a7cf8de417de5a588527b8515ad34a7
Daniel Sooknanan <71859934+sookiemonster@users.noreply.github.com>
2025-09-10 13:41:23 -0400

    Add OCR + Text Structuring  (#1)
    - Add ImageToTextParser, using Google Cloud Vision to do OCR on base64 encoded images
    - Add MenuStructurer, using Google Gemini API to parse base64 images + the OCR text to structure the raw OCR text

---
+++ language-model/menu-reader/custom_types/Menu.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from typing import List
+from enum import StrEnum
+
+
+class MenuSymbol(StrEnum):
+    GF = "gluten-free"
+    VGE = "vegetarian"
+    VGA = "vegan"
+
+
+@dataclass
+class MenuItem:
+    name: str
+    description: str
+    symbols: List[str]
+
+
+@dataclass
+class MenuSection:
+    section: str
+    description: str
+    items: List[MenuItem]
