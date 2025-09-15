020698ad4059bc0747885b8ab381a2044b34f4f5
Thomas Yu <72948692+thomasyu21@users.noreply.github.com>
2025-09-14 23:44:22 -0400

    dummy endpoint that calls test api (#3)

---
+++ server/src/test/java/com/allerfree/AllerFree/AllerFreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allerfree.AllerFree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllerFreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
