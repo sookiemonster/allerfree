d26674fdbc8a6ad4f1f3bdfc8ba2e09ec7ffeb57
thomasyu21 <thomasyu085@gmail.com>
2025-09-27 21:38:25 -0400

    JWT setup files that allow for JWT generation and authorization based on the presence of a JWT

---
+++ server/src/main/java/com/allerfree/AllerFree/config/AuthController.java
@@ -0,0 +1,23 @@
+package com.allerfree.AllerFree.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.allerfree.AllerFree.payload.response.JwtResponse;
+
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+public class AuthController {
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    //Generates a token based on given username and returns it
+    @PostMapping("/requestToken")
+    public ResponseEntity<?> requestJWT() throws Exception {
+        String token = jwtUtil.generateToken();
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+    
+}
