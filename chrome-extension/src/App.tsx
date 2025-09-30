5a7ae35f7d47d8670e58108aa32246f7651e3a5e
Kelly Lin <linkelly150@gmail.com>
2025-09-30 18:48:45 -0400

    Initial UI construction for extension

--- chrome-extension/src/App.tsx
+++ chrome-extension/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home"
 import Results from "./pages/Results"
 import Profiles from "./pages/Profiles"
@@ -10,13 +10,7 @@ function App() {
   	
 	return (
 		<>
-			<nav>
-				<Link to="/">Home</Link>
-				<Link to="/results">Results</Link>
-				<Link to="/profiles">Profiles</Link>
-				<Link to="/allergies">Allergies</Link>
-    		</nav>
-			<Routes> 
+			<Routes>
     			<Route path="/" element={<Home />} />
 				<Route path="/results" element={<Results />} />
 				<Route path="/profiles" element={<Profiles />} />
