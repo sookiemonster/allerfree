887e96aaf64dd70e57f0900fc2e844f8efe0fdc1
thomasyu21 <thomasyu085@gmail.com>
2025-10-19 20:25:21 -0400

    POST requests to FastAPI are now async

--- server/src/main/java/com/allerfree/AllerFree/controller/TestController.java
+++ server/src/main/java/com/allerfree/AllerFree/controller/TestController.java
@@ -10,6 +10,7 @@ import java.util.Set;
 import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -28,12 +29,15 @@ import com.allerfree.AllerFree.dto.MenuResponse;
 import com.allerfree.AllerFree.dto.MenuSection;
 import com.allerfree.AllerFree.dto.Profile;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 @RestController
 public class TestController {    
     @Autowired
     private WebClient webClient;
 
-    private final List<String> mimeTypes = Arrays.asList("image/jpeg", "image/png");
+    private final List<String> mimeTypes = Arrays.asList("image/jpeg", "image/png", "image/webp");
 
     @PostMapping("/detect")
     public ResponseEntity<MenuResponse> detectAllergens(@RequestBody AllergenDetection ad){
@@ -48,46 +52,52 @@ public class TestController {
             }
         }
 
-        MenuOutput aggregatedOutputs = new MenuOutput();
-        MenuRequest requestBody = new MenuRequest(); //Object for request body for LLM API
-        requestBody.setAllergies(allergySet);
+        long startTimeMilli = System.currentTimeMillis();
 
-        int imgCounter = 0;
-        for (Image img : ad.getImages()){ //Send one image at a time to LLM API
+        Flux<Image> imageFlux = Flux.fromIterable(ad.getImages());
+        Mono<List<MenuOutput>> results = imageFlux.flatMapSequential(image -> 
+            {
+                MenuRequest currentRequest = new MenuRequest(allergySet, image);
+                return webClient.post()
+                                .uri("/detect/menu_image/")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue(currentRequest).retrieve()
 
-            if (!Base64.isBase64(img.getBase64().getBytes())){ //Skip images not Base64 encoded
-                failed.put(imgCounter, "Image is not Base64 encoded");
-            }else if (!mimeTypes.contains(img.getMime_type())){ //Skip images with invalid mime types
-                failed.put(imgCounter,"Invalid mime type");
-            }else{ //Attempt to send image to LLM API
-                try{
-                    requestBody.setImage(img);
-                    //Make POST requests to LLM API and store in menuResults
-                    MenuOutput output = webClient.post().uri("/detect/menu_image/").contentType(MediaType.APPLICATION_JSON).bodyValue(requestBody).retrieve().bodyToMono(MenuOutput.class).block(); //Should make this call async
-                    aggregatedOutputs.combineSections(output.getSections()); //Aggregate results from LLM into one MenuResponse object
-                    successful.add(imgCounter);
-                }catch (Exception e){ //Temporary error handling
-                    failed.put(imgCounter,"SOMETHING WENT WRONG WITH POST REQUEST TO LLM API: " + e.getMessage());
-                }
-            }
+                                //Error Handling
+                                .onStatus(HttpStatusCode::isError, response -> 
+                                    response.bodyToMono(String.class)
+                                            .flatMap(e -> {
+                                                System.out.println("Something went wrong when POSTing to Menu Reader: " + e);
+                                                //failed.put(imgCounter,"Something went wrong when POSTing to Menu Reader: " + errorBody);
+                                                return Mono.empty(); // Skip failed request
+                                            })
+                                    )
+                                .bodyToMono(MenuOutput.class)
+                                .onErrorResume(e -> {
+                                    System.out.println("Something went wrong when POSTing to Menu Reader: " + e);
+                                    //failed.put(imgCounter,"Something went wrong when POSTing to Menu Reader: " + e);
+                                    return Mono.empty(); // Skip failed request
+                                });
+            })
+            .collectList(); //Collect all responses into a list
 
-            imgCounter++;
-        }
+        long endTimeMilli = System.currentTimeMillis();
+        long durationMilli = endTimeMilli - startTimeMilli;
+        System.out.println("Execution time in milliseconds: " + durationMilli);
 
         MenuResponse menuResults = new MenuResponse(); //Object to return to frontend
 
         //Parse + Format Response
         menuResults.setFailed(failed);
         menuResults.setSuccessful(successful);
-        menuResults.setResults(parseResponse(aggregatedOutputs, ad.getProfiles()));
+        menuResults.setResults(parseResponse(results.block(), ad.getProfiles()));
         return ResponseEntity.ok(menuResults);
-        
     }
     
     //Parses and formats aggregated responses from LLM API
         //Into a map where each profile gets its own menu
         //With allergy predictions specific to that profile (depending on allergies and sensitivity)
-    public HashMap<String, MenuOutput> parseResponse(MenuOutput menuResponse, Profile[] profiles){
+    public HashMap<String, MenuOutput> parseResponse(List<MenuOutput> menuResponse, Profile[] profiles){
         //Setup HashMap
         HashMap<String, MenuOutput> result = new HashMap<String, MenuOutput>();
         for (Profile prof : profiles){ //Seed map with profile names and empty menu outputs
@@ -97,35 +107,38 @@ public class TestController {
         HashSet<String> seenItems = new HashSet<String>(); //to handle duplicate menu images (and thus duplicate menu items)
 
         //Loop through all items in the LLM response
-        for (MenuSection section : menuResponse.getSections()){
-            //Create copy sections for each profile
-            for (Profile prof : profiles){
-                result.get(prof.getName()).addNewSection(section);
-            }
+        for (MenuOutput output : menuResponse){
+            for (MenuSection section : output.getSections()){
+                //Create copy sections for each profile
+                for (Profile prof : profiles){
+                    result.get(prof.getName()).addNewSection(section);
+                }
 
-            for (MenuItem item : section.getItems()){ //For every item in the menu
-                if (!seenItems.contains(item.getName())){ //Filter seen items
-                    for (Profile prof : profiles){ //Create copy items for each profiles respective menu
-                        List<MenuSection> tempSections = result.get(prof.getName()).getSections();
-                        tempSections.get(tempSections.size() - 1).addMenuItem(item, prof.getAllergens()); 
+                for (MenuItem item : section.getItems()){ //For every item in the menu
+                    if (!seenItems.contains(item.getName())){ //Filter seen items
+                        for (Profile prof : profiles){ //Create copy items for each profiles respective menu
+                            List<MenuSection> tempSections = result.get(prof.getName()).getSections();
+                            tempSections.get(tempSections.size() - 1).addMenuItem(item, prof.getAllergens()); 
+                        }
+                    }
+                    seenItems.add(item.getName());
+                }
+
+                //Remove empty sections
+                for (Profile prof : profiles){
+                    List<MenuSection> tempSections = result.get(prof.getName()).getSections();
+                    if (tempSections.get(tempSections.size() - 1).getItems().size() == 0){ //If last section has no MenuItems -> remove from sections list
+                        tempSections.remove(tempSections.size() - 1);
                     }
                 }
-                seenItems.add(item.getName());
-            }
 
-            //Remove empty sections
-            for (Profile prof : profiles){
-                List<MenuSection> tempSections = result.get(prof.getName()).getSections();
-                if (tempSections.get(tempSections.size() - 1).getItems().size() == 0){ //If last section has no MenuItems -> remove from sections list
-                    tempSections.remove(tempSections.size() - 1);
-                }
             }
-
         }
         return result;
     }
     
     
+    //TEMPORARY STUFF
     @ExceptionHandler
     public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
@@ -133,7 +146,7 @@ public class TestController {
 
     //JUST OUTPUTS LLM RESPONSE AGGREGATED (WITHOUT PARSING + FORMATTING)
     @PostMapping("/detect_test")
-    public ResponseEntity<MenuOutput> detectAllergensTest(@RequestBody AllergenDetection ad){
+    public ResponseEntity<MenuResponse> detectAllergensTest(@RequestBody AllergenDetection ad){
         //For input validation
         List<Integer> successful = new ArrayList<Integer>();
         HashMap<Integer, String> failed = new HashMap<Integer, String>();
@@ -148,6 +161,8 @@ public class TestController {
         MenuOutput aggregatedOutputs = new MenuOutput();
         MenuRequest requestBody = new MenuRequest(); //Object to serialize as request body for LLM API
         
+        long startTimeMilli = System.currentTimeMillis();
+        
         requestBody.setAllergies(allergySet);
         int imgCounter = 0;
         for (Image img : ad.getImages()){ //Send one image at a time to LLM API
@@ -160,7 +175,7 @@ public class TestController {
                 requestBody.setImage(img);
                 try{
                     //Make POST requests to LLM API and store in menuResults
-                    MenuOutput output = webClient.post().uri("/detect/menu_image/").contentType(MediaType.APPLICATION_JSON).bodyValue(requestBody).retrieve().bodyToMono(MenuOutput.class).block(); //Should make this call async?
+                    MenuOutput output = webClient.post().uri("/detect/menu_image/").contentType(MediaType.APPLICATION_JSON).bodyValue(requestBody).retrieve().bodyToMono(MenuOutput.class).block();
                     aggregatedOutputs.combineSections(output.getSections()); //Aggregate results from LLM into one MenuResponse object
                     successful.add(imgCounter);
                 }catch (Exception e){ //Temporary error handling
@@ -170,6 +185,10 @@ public class TestController {
 
             imgCounter++;
         }
+        long endTimeMilli = System.currentTimeMillis();
+        long durationMilli = endTimeMilli - startTimeMilli;
+        System.out.println("Execution time in milliseconds: " + durationMilli);
+
 
         MenuResponse menuResults = new MenuResponse(); //Object to deserialize response from LLM API
 
@@ -177,8 +196,46 @@ public class TestController {
         menuResults.setFailed(failed);
         menuResults.setSuccessful(successful);
         menuResults.setResults(parseResponse(aggregatedOutputs, ad.getProfiles()));
-        return ResponseEntity.ok(aggregatedOutputs);
+        return ResponseEntity.ok(menuResults);
         
+    }
+
+    public HashMap<String, MenuOutput> parseResponse(MenuOutput menuResponse, Profile[] profiles){
+        //Setup HashMap
+        HashMap<String, MenuOutput> result = new HashMap<String, MenuOutput>();
+        for (Profile prof : profiles){ //Seed map with profile names and empty menu outputs
+            result.put(prof.getName(), new MenuOutput());
+        }
+
+        HashSet<String> seenItems = new HashSet<String>(); //to handle duplicate menu images (and thus duplicate menu items)
+
+        //Loop through all items in the LLM response
+            for (MenuSection section : menuResponse.getSections()){
+                //Create copy sections for each profile
+                for (Profile prof : profiles){
+                    result.get(prof.getName()).addNewSection(section);
+                }
+
+                for (MenuItem item : section.getItems()){ //For every item in the menu
+                    if (!seenItems.contains(item.getName())){ //Filter seen items
+                        for (Profile prof : profiles){ //Create copy items for each profiles respective menu
+                            List<MenuSection> tempSections = result.get(prof.getName()).getSections();
+                            tempSections.get(tempSections.size() - 1).addMenuItem(item, prof.getAllergens()); 
+                        }
+                    }
+                    seenItems.add(item.getName());
+                }
+
+                //Remove empty sections
+                for (Profile prof : profiles){
+                    List<MenuSection> tempSections = result.get(prof.getName()).getSections();
+                    if (tempSections.get(tempSections.size() - 1).getItems().size() == 0){ //If last section has no MenuItems -> remove from sections list
+                        tempSections.remove(tempSections.size() - 1);
+                    }
+                }
+
+            }
+        return result;
     }
 
 }
