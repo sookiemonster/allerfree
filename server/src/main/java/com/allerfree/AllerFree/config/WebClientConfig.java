c14a47863b39f0056acd1fc422dbaa0fd45bdc5e
Daniel Sooknanan <71859934+sookiemonster@users.noreply.github.com>
2025-12-16 01:14:57 -0500

    Add Dockerfiles & docker compose. (#36)
    * Draft docker compose & server Dockerfile.
    
    * springboot dockerfile
    
    * Fix docker compose file & Dockerfile for language model.
    
    * Fix server Docker file. Setup docker compose for server service.
    
    * Remove docker label from dockerfile.
    
    ---------
    
    Co-authored-by: thomasyu21 <thomasyu085@gmail.com>

--- server/src/main/java/com/allerfree/AllerFree/config/WebClientConfig.java
+++ server/src/main/java/com/allerfree/AllerFree/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.allerfree.AllerFree.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,10 +8,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("${MODEL_URL}")
+    private String llmURL;
+
     @Bean
     WebClient webClient() {
         return WebClient.builder()
-                .baseUrl("http://127.0.0.1:8000/")
+                .baseUrl(llmURL)
                 .build();
     }
 
